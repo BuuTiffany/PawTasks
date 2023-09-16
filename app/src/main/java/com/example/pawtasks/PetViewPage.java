f7b20f4942ab01eb94bf588fafee857462e8c028
Nokk3n <dylan.werelius@gmail.com>
2023-09-15 19:16:45 -0700

    Created Basic App Framework
    Added all the basic skeleton for the app.
    
    The three pages can be navigated to and interacted with (although there is nothing to interact with currently)

---
+++ app/src/main/java/com/example/pawtasks/PetViewPage.java
@@ -0,0 +1,24 @@
+package com.example.pawtasks;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class PetViewPage extends Fragment {
+
+    public PetViewPage() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pet_view_page, container, false);
+    }
+}
